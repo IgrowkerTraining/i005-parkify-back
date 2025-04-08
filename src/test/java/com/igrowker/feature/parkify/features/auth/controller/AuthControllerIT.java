21c5a2492c4dcb19355a87f386d31572be159fd4
zapisator <zapisator@logan-club.ru>
2025-04-07 23:12:03 -0300

    fix(AuthControllerIT): Move ddl-auto DB create-grop product unsafe configuration to test class

--- src/test/java/com/igrowker/feature/parkify/features/auth/controller/AuthControllerIT.java
+++ src/test/java/com/igrowker/feature/parkify/features/auth/controller/AuthControllerIT.java
@@ -25,12 +25,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Testcontainers
 @TestPropertySource(properties = {
-        "jwt.secret=TXlUZXN0U2VjcmV0S2V5VGhhdElzTG9uZ0Vub3VnaDEyMw=="
+        "jwt.secret=TXlUZXN0U2VjcmV0S2V5VGhhdElzTG9uZ0Vub3VnaDEyMw==",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 @Sql(statements = {
         "DELETE FROM users WHERE email = 'test.owner.it@example.com';",
-        "INSERT INTO users (email, password, role) " +
-                "VALUES ('test.owner.it@example.com', " +
+        "INSERT INTO users (username, email, password, role) " +
+                "VALUES ('testuser', 'test.owner.it@example.com', " +
                 "'$2a$10$PK8hd/HO2R/mvqqhdyhS7.QjRF5AKbCMeclnVm.yV6tchnT/CkN6K', " +
                 "'OWNER'" +
                 ");"
