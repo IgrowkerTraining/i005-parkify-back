b048789ce35d4aae16169597c44b516b84dca6e7
maurifigueroa <mauricio.david.figueroa@gmail.com>
2025-04-21 19:53:22 -0300

    Cambia CORS para que acepte solicitudes de la web

--- src/main/java/com/igrowker/feature/parkify/config/WebConfig.java
+++ src/main/java/com/igrowker/feature/parkify/config/WebConfig.java
@@ -13,8 +13,11 @@ public class WebConfig implements WebMvcConfigurer{
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // Usar la variable de entorno para obtener el frontend URL
+                String frontendUrl = System.getenv("FRONTEND_URL");
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)  // Establecer el frontend URL dinámicamente
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
