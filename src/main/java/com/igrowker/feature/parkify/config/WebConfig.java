b6b1b94ad644b287e677ed3f6baafb5aabb923de
maurifigueroa <mauricio.david.figueroa@gmail.com>
2025-04-22 18:41:29 -0300

    test modificando application.properties para prod

--- src/main/java/com/igrowker/feature/parkify/config/WebConfig.java
+++ src/main/java/com/igrowker/feature/parkify/config/WebConfig.java
@@ -17,9 +17,9 @@ public class WebConfig implements WebMvcConfigurer{
                 String frontendUrl = System.getenv("FRONTEND_URL");
                 System.out.println("FRONTEND_URL: " + frontendUrl);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://34.107.135.109", "http://localhost")
+                        //.allowedOrigins("http://34.107.135.109", "http://localhost")
                         //.allowedOrigins("*")
-                        //.allowedOrigins(frontendUrl)  // Establecer el frontend URL dinámicamente
+                        .allowedOrigins(frontendUrl)  // Establecer el frontend URL dinámicamente
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
